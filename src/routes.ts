ebd39695a6783952773edb725c55b80eb3a4f90c
RenanEstery20 <renanestery1@gmail.com>
2022-02-11 14:36:13 -0300

    get production

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { AuthenticateDeliverymanController } from './modules/account/authenticat
 import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController'
 import { ensureAuthenticateClient } from './middlewares/ensureAuthenticateClient'
 import { FindAllAvailableController } from './modules/deliveries/useCases/findAllAvailable/FindAllAvailableController'
+import { ensureAuthenticateDeliveryman } from './middlewares/ensureAuthenticateDeliveryman'
 
 const routes = Router()
 
@@ -29,6 +30,10 @@ routes.post(
   ensureAuthenticateClient,
   createDeliveryController.handle
 )
-routes.get('/delivery/available', findAllAvailableController.handle)
+routes.get(
+  '/delivery/available',
+  ensureAuthenticateDeliveryman,
+  findAllAvailableController.handle
+)
 
 export { routes }
